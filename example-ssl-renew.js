6a40e6fa1d5246d72cde020e01ada216d93d626e
Anthony Scinocco <scinocco.a@gmail.com>
2017-06-29 12:19:54 -0400

    angering meaning of rule

--- example-ssl-renew.js
+++ example-ssl-renew.js
@@ -4,7 +4,7 @@ let exec = require('child_process').exec;
 console.log('Creating rules...');
 
 let rule = new schedule.RecurrenceRule();
-rule.minute = 1; // every year
+rule.minute = 21; // every year
 
 console.log('Defining commands...');
 let command = 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt';

3d483118a7d0fc69272ee587288f3e40c4fcb68c
Anthony Scinocco <scinocco.a@gmail.com>
2017-06-29 13:21:25 -0400

    configured actual script

--- node-ssl-schedule.js
+++ node-ssl-schedule.js
@@ -1,5 +1,5 @@
 let schedule = require('node-schedule');
-let exec = require('child_process').exec;
+let shell    = require('shelljs');
 
 console.log('Creating rules...');
 
@@ -7,11 +7,12 @@ let rule = new schedule.RecurrenceRule();
 rule.year = 1; // every year
 
 console.log('Defining commands...');
-let command = 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt';
+let command = 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/C=CA/ST=Denial/L=King/O=Dis/CN=localhost"';
 
 console.log('Setting jobs...');
+
 let renewSSLJob = schedule.scheduleJob(rule, function () {
     console.log('Renewing SSL cert...');
-    exec(command);
+    shell.exec(command);
     console.log('SSL cert renewed...');
 });

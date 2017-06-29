8ee9d02c8c23f2136c94cb28dacfa4641495572a
Anthony Scinocco <scinocco.a@gmail.com>
2017-06-29 13:40:00 -0400

    it was the recurrence i was using

--- node-cert-renewal.js
+++ node-cert-renewal.js
@@ -4,7 +4,7 @@ let shell = require('shelljs');
 console.log('Creating rules...');
 
 let rule = new schedule.RecurrenceRule();
-rule.year = 1; // every year
+rule.month = 6; // every year
 
 console.log('Defining commands...');
 let command = 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/C=CA/ST=Denial/L=King/O=Dis/CN=localhost"';
@@ -13,16 +13,5 @@ console.log('Setting jobs...');
 let renewSSLJob = schedule.scheduleJob(rule, function () {
     console.log('Renewing SSL cert...');
     shell.exec(command);
-    shell.echo('Shell is echoing!');
     console.log('SSL cert renewed...');
-});
-
-// lets figure out why the script ends
-
-let testRule = new schedule.RecurrenceRule();
-testRule.minute = 40;
-
-let testJob = schedule.scheduleJob(testRule, function () { 
-    console.log('Console log');
-    shell.exec('Shell Exec');
 });

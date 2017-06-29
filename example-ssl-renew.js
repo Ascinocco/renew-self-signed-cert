f74d180ec16f570ab8ebadf2d471ab8c737c2697
Anthony Scinocco <scinocco.a@gmail.com>
2017-06-29 12:10:14 -0400

    switched to recurring job

--- example-ssl-renew.js
+++ example-ssl-renew.js
@@ -1,23 +1,17 @@
 let schedule = require('node-schedule');
 let exec = require('child_process').exec;
 
-// console.log('Creating rules...');
+console.log('Creating rules...');
 
-// let rule = new schedule.RecurrenceRule();
-// rule.year = 1; // every year
+let rule = new schedule.RecurrenceRule();
+rule.minute = 1; // every year
 
 console.log('Defining commands...');
 let command = 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt';
 
-let j = schedule.scheduleJob({minute: 1}, function() {
-    console.log('Job is running...');
+console.log('Setting jobs...');
+let renewSSLJob = schedule.scheduleJob(rule, function () {
+    console.log('Renewing SSL cert...');
     exec(command);
-    console.log('Self signed cert regenerated...');
+    console.log('SSL cert renewed...');
 });
-
-// console.log('Setting jobs...');
-// let renewSSLJob = schedule.scheduleJob(rule, function () {
-//     console.log('Renewing SSL cert...');
-//     exec(command);
-//     console.log('SSL cert renewed...');
-// });

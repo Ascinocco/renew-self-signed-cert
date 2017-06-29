609a038c02445172919c311ad8a7dbbfc96c2320
Anthony Scinocco <scinocco.a@gmail.com>
2017-06-29 12:57:57 -0400

    testing attention pls

--- example-ssl-renew.js
+++ example-ssl-renew.js
@@ -4,7 +4,7 @@ let exec = require('child_process').exec;
 console.log('Creating rules...');
 
 let rule = new schedule.RecurrenceRule();
-rule.minute = 53; // every year
+rule.minute = 59; // every year
 
 console.log('Defining commands...');
 let command = 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt';
@@ -12,6 +12,7 @@ let command = 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout 
 console.log('Setting jobs...');
 let renewSSLJob = schedule.scheduleJob(rule, function () {
     console.log('Renewing SSL cert...');
-    exec(command);
+    console.log(command);
+    exec('pwd');
     console.log('SSL cert renewed...');
 });

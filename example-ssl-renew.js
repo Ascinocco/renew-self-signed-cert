7c3f89e7e58e54bc692d02161f573e50760692e6
Anthony Scinocco <scinocco.a@gmail.com>
2017-06-29 13:02:24 -0400

    reset timer

--- example-ssl-renew.js
+++ example-ssl-renew.js
@@ -4,7 +4,7 @@ let shell = require('shelljs');
 console.log('Creating rules...');
 
 let rule = new schedule.RecurrenceRule();
-rule.minute = 59; // every year
+rule.minute = 5; // every year
 
 console.log('Defining commands...');
 let command = 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt';
